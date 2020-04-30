b5e3753a60b73b370715f9c3d88e660663973cc4
Ubuntu <ubuntu@ip-172-31-87-149.ec2.internal>
2020-04-30 20:02:45 +0000

    Developer years function added in JS file.

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+
+    // Website authors developer years appear in the footer
+     
+    $('#developer-years').text('2018 - ' + new Date().getFullYear());
+    
+});
